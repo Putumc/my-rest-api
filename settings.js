93db1a7ad0443109efae30f9907ad693e0432c45
DANZ store <142219160+Putumc@users.noreply.github.com>
2024-04-20 15:09:16 +0800

    settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ global.creator = 'rianofc'// yourname
 global.MONGO_DB_URI = "mongodb+srv://mcariawan:api2352@cluster0.whnuqrl.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "mcariawan@gmail.com" //email
-global.email_password = "aspz ahxh zgjr wpap" //application password email
+global.email_password = "ofrh sxsq dnoc irza" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
